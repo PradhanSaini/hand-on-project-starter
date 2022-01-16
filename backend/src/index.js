55f2d0630f57baad730ff48286b02e887df81ea6
PradhanSaini <pradhansainiiit@gmail.com>
2022-01-17 01:10:49 +0530

    frontend setup initialised

---
+++ backend/src/index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv=require("dotenv");
+dotenv.config();
+
+mongoose.connect(process.env.MONGODB_URL).then(()=>{
+    console.log("DB is connected .......");
+}).catch((err)=>{
+    console.log(err);
+})
+
+const app=express();
+
+app.get("/hello-world",(req,res)=>{
+    res.send("Hello Pradhan");
+})
+
+app.listen(process.env.PORT || 3000,()=>{
+    console.log(`PORT ${process.env.PORT} is running ......`)
+})
+
+
